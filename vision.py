6d229e371ed7b200617179e837c50c51aef71e09
MegaFlop10 <megaflop10@hotmail.com>
2017-01-29 17:35:20 +1100

    Removed timer and debug code, added angle calculations.

--- vision.py
+++ vision.py
@@ -1,15 +1,20 @@
 import numpy as np
 import cv2
 from networktables import NetworkTables
-import time
+import math
+
+
+# Constants
+FOCAL_LENGTH = 939.965898323  # Focal length of the Microsoft LifeCam HD3000
+IMAGE_WIDTH = 1280  # Pixel width of the Microsoft LifeCam HD3000 (Apparently)
+
+# Define threshold values (H, S, V)
+THRESH_MIN = np.array([50, 50, 0], np.uint8)
+THRESH_MAX = np.array([80, 255, 255], np.uint8)
 
 
 # Converts to HSV, then filters the image so only green remains.
 def threshold():
-    # Define threshold values (H, S, V)
-    THRESH_MIN = np.array([50, 50, 0], np.uint8)
-    THRESH_MAX = np.array([80, 255, 255], np.uint8)
-
     # Convert to HSV format
     hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
@@ -25,17 +30,17 @@ def find_target():
     contours, hierarchy = cv2.findContours(edge, mode=cv2.RETR_LIST, method=cv2.CHAIN_APPROX_NONE)
 
     # Select largest contour
-    largestArea = 0
+    largest_area = 0
     for c in contours:
         area = cv2.contourArea(c)
-        if area > largestArea:
-            largestArea = area
-            largestContour = c
+        if area > largest_area:
+            largest_area = area
+            largest_contour = c
 
     # Try to draw the largest contour over the original image, unless there is no contour
     try:
-        cv2.drawContours(img, largestContour, -1, (0, 0, 255), thickness=3)
-        return largestContour
+        cv2.drawContours(img, largest_contour, -1, (0, 0, 255), thickness=3)
+        return largest_contour
     except UnboundLocalError:
         pass
 
@@ -59,25 +64,26 @@ def find_coordinates(target_contour):
 
 # Find angle to the target
 def find_angle(x_target):
-    pass                                                                                                              
+    image_centre = IMAGE_WIDTH / 2
+    angle = math.atan((x_target - image_centre) / FOCAL_LENGTH)  # Formula based on 254's presentation
+    return angle
 
 
 # Called when 'request' changes
 def handle_request(table, key, value, isNew):
     print("Request Received")
-    if (key == 'request' and value == True):
-            table.putNumber('targetAngle', x) # x & y are not actually angle & distance yet
+    if key == 'request' and value is True:
+            table.putNumber('targetAngle', targetAngle)  # x & y are not actually angle & distance yet
             table.putNumber('targetDistance', y)
-            table.putBoolean('request',False)
+            table.putBoolean('request', False)
 
 
-NetworkTables.initialize(server='roboRIO-9985-frc.local')
-table =  NetworkTables.getTable("vision")
+NetworkTables.initialize(server='roboRIO-9985-frc.local')  # TODO: Set to static IP addresses
+table = NetworkTables.getTable("vision")
 table.addTableListener(handle_request)
 
 # Start video capture with camera 0
 cap = cv2.VideoCapture(0)
-start_tick=time.time()
 while True:
     # Read image
     retval, img = cap.read()
@@ -89,20 +95,10 @@ while True:
     # Find its coordinates
     x, y = find_coordinates(target)
     # Find angle of target
+    targetAngle = find_angle(x)
     
-
     # Display original image, which has the contour and x, y drawn onto it
     cv2.imshow("Video Capture", img)
     cv2.waitKey(1)
 
     table = NetworkTables.getTable('vision')
-    count_tick = time.time() - start_tick
-    if (count_tick > 10.0):
-        print("10s passed")
-        print(table.getBoolean('request'))
-        
-        #print(" targetAngle="+table.getNumber('targetAngle')+" targetDistance="+table.getNumber('targetDistance')+"\n")
-        
-        start_tick=time.time()   
-
-    
